a412cf7194df2e5870b2f3c772c9ab18ede888c6
Chonthicha <154409773+6210110062ch@users.noreply.github.com>
2024-06-21 17:02:16 +0700

    addbackend

---
+++ backend-expressslipcheck/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', async(req, res) => {
+    res.json({"msg": "Hello World"});
+
+})
+
+app.listen(port, () =>{
+    console.log('Server running at port: ${port}');
+
+});
